f22fb6316ade495f9115ae28e2aaab5021ecefe8
xxx <ilyasovnurgiz@yandex.ru>
2024-07-31 16:33:04 +0500

    Итоговая сумма return

--- src/Basket.java
+++ src/Basket.java
@@ -1,22 +1,31 @@
 public class Basket {
 
     public static String items = "";
+    public static int totalPrice = 0;
 
     public static void main(String[] args) {
         add("Колбаса", 76);
         add("Молоко",91);
         add("Масло",134);
         print("Содержимой карзины: ");
+        System.out.println(getTotalPrice());
         clear();
         print("Cодержимое карзины ");
+        System.out.println(getTotalPrice());
     }
 
     public static void add(String name, int price) {
         items = items + "\n" + name + "-" + price;
+        totalPrice = totalPrice + price;
     }
 
     public static void clear() {
         items = "";
+        totalPrice = 0;
+    }
+
+    public static int getTotalPrice() {
+        return totalPrice;
     }
 
     public static void print(String title) {
